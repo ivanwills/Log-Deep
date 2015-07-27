f0eb63aa1d877099d8a4936a768268e259f9b6ac
Ivan Wills <ivan.wills@gmail.com>
2015-07-27 16:45:36 +1000

    Updating for release of version 0.3.5

--- lib/Log/Deep.pm
+++ lib/Log/Deep.pm
@@ -19,7 +19,7 @@ use Fcntl qw/SEEK_END/;
 use English qw/ -no_match_vars /;
 use base qw/Exporter/;
 
-our $VERSION     = version->new('0.3.4');
+our $VERSION     = version->new('0.3.5');
 
 Readonly my @LOG_LEVELS => qw/info message debug warn error fatal/;
 
@@ -425,7 +425,7 @@ Log::Deep - Deep Logging of information about a script state
 
 =head1 VERSION
 
-This documentation refers to Log::Deep version 0.3.4.
+This documentation refers to Log::Deep version 0.3.5.
 
 
 =head1 SYNOPSIS
